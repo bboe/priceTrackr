71d39290f5ee5ba34c549db6830b4cbea2911713
Bryce Boe <bbzbryce@gmail.com>
2009-10-14 18:40:41 +0000

    fixed contact form

--- document_root/contact_form.php
+++ document_root/contact_form.php
@@ -1,72 +1,60 @@
 <?php
 ob_start();
 define('START', true);
-$title = 'Contact Form';
 require_once 'includes/header.php';
-
-if (!isset($_GET['type'])) {
-	header('Location: /');
-	exit();
-}
-$type = $_GET['type'];
-$types = array('general','support','marketing');
-if (!in_array($type,$types)) header('Location: /');
 
 $from = '';
 $message = '';
 $subject = '';
 
 if (isset($_POST) && sizeof($_POST) > 0) {
-	set_magic_quotes_runtime(FALSE);
-	if (get_magic_quotes_gpc()) {
-		$_POST = stripslashes_array($_POST);
-	}
-	$mesg = '';
-	if (isset($_POST['submit']) && $_POST['submit'] == "submit") {
-		$valid = true;
-		if (!false) {
-			$from = trim($_POST['from']);
-			$subject = trim($_POST['subject']);
-			$message = trim($_POST['message']);
-			if ($from == '') {
-				$valid = false;
-				$mesg .= '<p class="mesg">Error: No from address</p>';
-			}
-			if (!validateEmail($from)) {
-				$valid = false;
-				$mesg .= '<p class="mesg">Error: Invalid email address</p>';
-			}
-			if ($subject == '') {
-				$valid = false;
-				$mesg .= '<p class="mesg">Error: No subject</p>';
-			}
-			if ($message == '') {
-				$valid = false;
-				$mesg .= '<p class="mesg">Error: No message</p>';
-			}
-		}
-		if ($valid) {
-			$to = $type . '@pricetrackr.com';
-			$to = 'bbzbryce@gmail.com';
-			sendPearMail($to,$subject,$message,$from);
-			header('Location: /thankyou.php');
-			exit();
-		}
-	}
-	else {
-		header('Location: /');
-		exit();
-	}
-}
+  set_magic_quotes_runtime(FALSE);
+  if (get_magic_quotes_gpc()) {
+    $_POST = stripslashes_array($_POST);
+  }
+  $msg = '';
+  if (isset($_POST['submit']) && $_POST['submit'] == "submit") {
+    $valid = true;
+    if (!false) {
+      $from = trim($_POST['from']);
+      $subject = trim($_POST['subject']);
+      $message = trim($_POST['message']);
+      if ($from == '') {
+	$valid = false;
+	$msg .= '<p class="msg">Error: No from address</p>';
+      }
+      else if (!validateEmail($from)) {
+	$valid = false;
+	$msg .= '<p class="msg">Error: Invalid email address</p>';
+      }
+      if ($subject == '') {
+	$valid = false;
+	$msg .= '<p class="msg">Error: No subject</p>';
+      }
+      if ($message == '') {
+	$valid = false;
+	$msg .= '<p class="msg">Error: No message</p>';
+      }
+    }
+    if ($valid) {
+      $to = 'admin@pricetrackr.com';
+      sendPearMail($to, $subject, $message, $from);
+      $msg .= '<p class="msg">Your message has been sent. Thank you.</p>';
+    }
+  }
+  else {
+    header('Location: /');
+    exit();
+  }
+ }
 ob_end_flush();
-$Type = ucfirst($type);
 ?>
 
-<h1><?=$Type?> Contact Form</h1>
+<h1>Contact</h1>
 
-<?php if (isset($mesg)) print $mesg; ?>
+<?php if (isset($msg)) print $msg; ?>
 
-<form name="contact" action="/c/<?=$type?>" method="post">
+<form name="contact" action="/contact/" method="post">
   <p>
     <label for="from">your email:</label>
     <br />
