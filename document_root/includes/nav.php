b6f9a4dc4589208712fca39a7a34bbfdd3670205
Bryce Boe <bbzbryce@gmail.com>
2009-10-21 06:25:29 +0000

    no more item page

--- document_root/includes/nav.php
+++ document_root/includes/nav.php
@@ -3,11 +3,9 @@
     <ul id="nav">
     	<?php ($_SERVER['SCRIPT_NAME'] == '/index.php')?$class = ' class="active"':$class=''; ?>
 	    <li><a href="/"<?php echo $class; ?>>Home</a></li>
-	    <?php ($_SERVER['SCRIPT_NAME'] == '/daily.php')?$class = ' class="active"':$class=''; ?>
-		<li><a href="/daily/"<?php echo $class; ?>>Daily Drops</a></li>
-    	<?php ($_SERVER['SCRIPT_NAME'] == '/add.php')?$class = ' class="active"':$class=''; ?>
+    	<?php ($_SERVER['SCRIPT_NAME'] == '/faq.php')?$class = ' class="active"':$class=''; ?>
 		<li><a href="/faq/"<?php echo $class; ?>>FAQ</a></li>
-    	<?php ($_SERVER['SCRIPT_NAME'] == '/contact.php')?$class = ' class="active"':$class=''; ?>
+    	<?php ($_SERVER['SCRIPT_NAME'] == '/contact_form.php')?$class = ' class="active"':$class=''; ?>
 		<li><a href="/contact/"<?php echo $class; ?>>Contact</a></li>
     	<?php ($_SERVER['SCRIPT_NAME'] == '/about.php')?$class = ' class="active"':$class=''; ?>
 		<li><a href="/about/"<?php echo $class; ?>>About</a></li>
