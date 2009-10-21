8009ee10e36ba6a46e741b0fa7e0882241e64a96
Bryce Boe <bbzbryce@gmail.com>
2009-10-21 22:12:00 +0000

    nginx + fastcgi working now

--- php_root/includes/header.php
+++ php_root/includes/header.php
@@ -14,6 +14,7 @@ PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 <head>
 <title>priceTrackr - <?php require_once 'title.php'?></title>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+<link rel="shortcut icon" href="/favicon.ico" />
 <link rel="stylesheet" type="text/css" href="/style/layout.css" />
 <script type="text/javascript" src="/scripts/niftycube.js"></script>
 <script type="text/javascript">
