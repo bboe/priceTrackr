d8513fc3b79f5cb40d6ac743e0816bba30db889c
Bryce Boe <bbzbryce@gmail.com>
2009-10-22 20:24:48 +0000

    Cleanup error stuff

--- php_root/includes/myDB.php
+++ php_root/includes/myDB.php
@@ -5,15 +5,10 @@ class myDB {
   private $db;
   
   function myDB() {
-    // Set PEAR Error Handling function
-    if (defined('ERROR_SET') && ERROR_SET)
-      PEAR::setErrorHandling (PEAR_ERROR_CALLBACK, 'pear_error_handler');
-
     $dsn = 'mysqli://pt_user:pritshiz@localhost/priceTrackr';
     $options = array('debug' => 2, 'result_buffering' => false);
     $this->db =& MDB2::factory($dsn, $options);
     
-    // This only happens if ERROR_SET is not TRUE
     if (PEAR::isError($this->db)) {
       // TODO: This should display the footer
       die('Sorry the page could not be processed at this time.');
@@ -24,17 +19,7 @@ class myDB {
   function prepare_execute($sql, $args, $types, $select, $allowError) {
     $prep = $this->db->prepare($sql, $types,
 			       $select?MDB2_PREPARE_RESULT:MDB2_PREPARE_MANIP);
-
-    // Disable error handling if we need to catch it.
-    if (defined('ERROR_SET') && ERROR_SET && $allowError)
-      PEAR::setErrorHandling(PEAR_ERROR_RETURN);
-
     $result = $prep->execute($args);
-    
-    // Reenable error handling
-    if (defined('ERROR_SET') && ERROR_SET && $allowError)
-      PEAR::setErrorHandling (PEAR_ERROR_CALLBACK, 'pear_error_handler');
-
     $prep->free();
     if ($allowError && PEAR::isError($result))
       return false;
@@ -79,9 +64,4 @@ class myDB {
   }
 }
 
-function pear_error_handler ($err_obj) {
-  // Trigger a generic PHP error so email reporting happens.
-  $error_string = $err_obj->getMessage() . '<br>' . $err_obj->getDebugInfo();
-  trigger_error($error_string, E_USER_ERROR);
-}
 ?>
--- php_root/search.php
+++ php_root/search.php
@@ -12,29 +12,33 @@ $start = microtime(true);
 if (isset($_GET['q'])) {
   $q = trim($_GET['q']);
   $len = strlen($q);
-  if ($len == 0) print '<p class="error">No search terms entered.</p>';
-  else if ($len < 2) print '<p class="error">Search string too short.</p>';
-  else if ($len > 100) print '<p class="error">Search string too long.</p>';
+  if ($len == 0) print '<p class="mesg">No search terms entered.</p>';
+  else if ($len > 100) print '<p class="mesg">Search string too long.</p>';
   else {
-	  $result = $db->prepare_execute('select newegg_id, title, match(title, model) against (? in boolean mode) as score from item where match(title, model) against (? in boolean mode) order by score desc limit 30', array($q, $q), array('text', 'text'), true, false);
-	  $time = microtime(true)-$start;
-	  print '<p class="searchTime">'. $time . ' seconds</p>';
-	  print '<p class="query">Query:<i>'.$q.'</i></p>';
-	  $size = sizeof($result);
-	  if ($size == 0) {
-	    print '<p>No results found. Please <a href="/contact/">contact us</a> if you believe priceTrackr is missing something.</p>';
-	  }
-	  else if ($size == 1) {
-	    header('Location: /i/'.$result[0]['newegg_id'].'/');
-	    exit();
-	  }
-	  else {
-	    print '<div class="results"><ol>';
-	    foreach ($result as $value) {
-	      print '<li><a href="/i/'.$value['newegg_id'].'/">'.$value['title'].'</a></span></li>'."\n";
-	    }
-	    print '</ol></div>';
-	  }
+    $terms = '';
+    foreach (split(' ', $q) as $i) {
+      if (strlen($i) < 2) $terms .= " '$i'";
+    }
+    if ($terms != '') print '<p class="mesg">The following terms have been ignored:'. $terms . '</p>';
+    $result = $db->prepare_execute('select newegg_id, title, match(title, model) against (? in boolean mode) as score from item where match(title, model) against (? in boolean mode) order by score desc limit 30', array($q, $q), array('text', 'text'), true, false);
+    $time = microtime(true)-$start;
+    print '<p class="searchTime">'. $time . ' seconds</p>';
+    print '<p class="query">Query:<i>'.$q.'</i></p>';
+    $size = sizeof($result);
+    if ($size == 0) {
+      print '<p>No results found. Please <a href="/contact/">contact us</a> if you believe priceTrackr is missing something.</p>';
+    }
+    else if ($size == 1) {
+      header('Location: /i/'.$result[0]['newegg_id'].'/');
+      exit();
+    }
+    else {
+      print '<div class="results"><ol>';
+      foreach ($result as $value) {
+	print '<li><a href="/i/'.$value['newegg_id'].'/">'.$value['title'].'</a></span></li>'."\n";
+      }
+      print '</ol></div>';
+    }
   }
  }
 ob_end_flush();
